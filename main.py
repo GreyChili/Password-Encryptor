5351e75853e021e928d20c47be147322c28980b8
GreyChili <greychili123@gmail.com>
2020-03-22 14:01:00 +0100

    Version 1

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import bcrypt
 
+filePath = input("Enter path to password destination file.\n")
+
+passF = open(filePath, "w+")
+
+test = "test"
+
+print("Password destination file set to:\n%s" %(filePath))
+
+passUnencrypted = input("Enter a password to encrypt:\n")
+
+def Encrypt(unencryptedPass):
+    unencryptedPass = unencryptedPass.encode()
+
+    encryptedPass = bcrypt.hashpw(unencryptedPass, bcrypt.gensalt())
+
+    encryptedPass = encryptedPass.decode()
+
+    return encryptedPass
+
+passF.write(Encrypt(passUnencrypted))
+passF.close()
+
+print("Done\nEncrypted password in:\n\n%s" %(filePath))
