f8a0809c46106f6ec02e1a74d565e0c5179a49f6
GreyChili <58607125+GreyChili@users.noreply.github.com>
2020-03-22 15:05:38 +0100

    implemented argparse

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import bcrypt
+import  argparse
+# pip install agrparse and bcrypt
 
-filePath = input("Enter path to password destination file.\n")
-
-passF = open(filePath, "w+")
-
-test = "test"
-
-print("Password destination file set to:\n%s" %(filePath))
-
-passUnencrypted = input("Enter a password to encrypt:\n")
+parser = argparse.ArgumentParser(description="Encrypt passwords with bcrypt")
+parser.add_argument("-p", "--password", type=str, metavar="", required=True, help="Password to encrypt")
+parser.add_argument("-f", "--file", type=str, metavar="", required=False, help="Save password to given file")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-q", "--quiet", action="store_true", help="print quiet")
+group.add_argument("-v", "--verbose", action="store_true", help="print verbose")
+args = parser.parse_args()
 
 def Encrypt(unencryptedPass):
     unencryptedPass = unencryptedPass.encode()
-
     encryptedPass = bcrypt.hashpw(unencryptedPass, bcrypt.gensalt())
-
     encryptedPass = encryptedPass.decode()
 
     return encryptedPass
 
-passF.write(Encrypt(passUnencrypted))
-passF.close()
+if __name__ == "__main__":
+    encyptedPass = Encrypt(args.password)
 
-print("Done\nEncrypted password in:\n\n%s" %(filePath))
+    if args.file != None:
+        f = open(args.file, "w")
+        f.write(encyptedPass)
+    else:
+        pass
+
+    message = None
+
+    if args.quiet:
+        message = encyptedPass
+    elif args.verbose:
+        message = "Unencrypted password is:\n%s\nEncrypted password is:\n%s" %(args.password, encyptedPass)
+    else:
+        message = "Encrypted password is:\n%s" % (encyptedPass)
+
+    print(message)
