aa2e9e788d66dbb96f82200b3b404f75cc8088b9
GreyChili <58607125+GreyChili@users.noreply.github.com>
2020-03-23 11:18:25 +0100

    Argparse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import  argparse
+import  argparse, bcrypt
+# pip install argparse and bcrypt
 
 parser = argparse.ArgumentParser(description="Encrypt passwords with bcrypt")
 parser.add_argument("-p", "--password", type=str, metavar="", required=True, help="Password to encrypt")
@@ -8,8 +9,23 @@ group.add_argument("-q", "--quiet", action="store_true", help="print quiet")
 group.add_argument("-v", "--verbose", action="store_true", help="print verbose")
 args = parser.parse_args()
 
+def Compare(inputPass, targetPassEncrypted):
+
+    if bcrypt.checkpw(inputPass.encode(), targetPassEncrypted):
+        return True
+    else:
+        return False
+
+def Encrypt( unencryptedPass):
+
+    unencryptedPass = unencryptedPass.encode()
+    encryptedPass = bcrypt.hashpw(unencryptedPass, bcrypt.gensalt())
+    encryptedPass = encryptedPass.decode()
+
+    return encryptedPass
+
 if __name__ == "__main__":
-    encyptedPass = pfb.Encrypt(args.password)
+    encyptedPass = Encrypt(args.password)
 
     if args.file != None:
         f = open(args.file, "w")
