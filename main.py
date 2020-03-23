113e4826e4c737c4b10303e3325a0173a74befc5
GreyChili <58607125+GreyChili@users.noreply.github.com>
2020-03-23 15:49:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import  argparse, bcrypt
 parser = argparse.ArgumentParser(description="Encrypt passwords with bcrypt")
 parser.add_argument("-p", "--password", type=str, metavar="", required=True, help="Password to encrypt")
 parser.add_argument("-f", "--file", type=str, metavar="", required=False, help="Save password to given file")
+parser.add_argument("-c", "--compare", type=str, metavar="", required=False, help="Compare password with an encrypted string")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-q", "--quiet", action="store_true", help="print quiet")
 group.add_argument("-v", "--verbose", action="store_true", help="print verbose")
@@ -33,12 +34,16 @@ if __name__ == "__main__":
     else:
         pass
 
+    
+
     message = None
 
     if args.quiet:
         message = encyptedPass
     elif args.verbose:
         message = "Unencrypted password is:\n%s\nEncrypted password is:\n%s" %(args.password, encyptedPass)
+    elif args.compare != None:
+        if Compare(args.password, args.compare)
     else:
         message = "Encrypted password is:\n%s" % (encyptedPass)
 
